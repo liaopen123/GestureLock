63aceaccb992d99bda58d7338e0583588de31c3e
廖鹏辉 <467969@qq.com>
2016-11-21 22:18:02 +0800

    Initial commit

---
+++ app/src/main/java/com/xiaoziqianbao/gesturelock/view/GestureLockView.java
@@ -0,0 +1,26 @@
+package com.xiaoziqianbao.gesturelock.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by liaopenghui on 2016/11/21.
+ */
+
+public class GestureLockView extends View {
+    public GestureLockView(Context context) {
+        super(context);
+    }
+
+    public GestureLockView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GestureLockView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    
+}
